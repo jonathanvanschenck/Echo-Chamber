481141c739e8e6d1983190076c7e07af01313a1a
jonathanvanschenck <44685047+jonathanvanschenck@users.noreply.github.com>
2019-12-08 15:57:16 -0800

    Add files via upload

--- app/static/js/echo.js
+++ app/static/js/echo.js
@@ -1,1 +1,51 @@
+$(document).ready(function() {
+    // Select the namespace for socket multiplexing
+    namespace = '/test';
+    
+    // Create socket connection
+    var socket = io(namespace);
+    
+    // Setup Event Handlers
+    // Connect
+    socket.on('connect', function() {
+        socket.emit('my_event', {data: 'I\'m connected'});
+    });
+    
+    // Recieved text
+    socket.on('my_response', function(msg, cb) {
+        // Create new <div></div> element
+        var newmsg = $('<div/>');
+        
+        // Stick some text into the middle <div>text</div>
+        newmsg.text('Received: ' + msg.data);
+        
+        // Convert the element into HTML text, then attached it to
+        //   the <div id='log'> item already in the page in the "contents"
+        //   section  
+        $('#log').append('<br>' + newmsg.html());
+        
+        // run cb
+        if (cb)
+            cb();
+        
+    });
+    
+    // Ping
+    
+    // Forms
+    $('form#emit').submit(function(event) {
+        socket.emit('my_event', {data: $('#emit_data').val()});
+        return false;
+    });
 
+    $('form#broadcast').submit(function(event) {
+        socket.emit('my_broadcast_event', {data: $('#broadcast_data').val()});
+        return false;
+    });
+    
+    $('form#disconnect').submit(function(event) {
+        socket.emit('disconnect_request');
+        return false;
+    });
+    
+});
